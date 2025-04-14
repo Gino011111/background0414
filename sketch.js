e33e1b9dd27a232837329138b71555dbd4d35d6c
Gino011111 <konjac1212@gmail.com>
2025-04-14 11:18:39 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,46 @@
+const NUM_RECTS = 10000;
+let rects = [];
+
+Q5.WebGPU();
+
+function setup() {
+	createCanvas();
+
+	for (let i = 0; i < NUM_RECTS; i++) {
+		rects.push({
+			x: 0,
+			y: 0,
+			size: 1,
+			speed: random(0.001, 0.02)
+		});
+	}
+
+	rectMode(CENTER);
+}
+
+function draw() {
+	background(0.1, 0.2);
+
+	fill(0.5, 0.9, 1, 0.1);
+	noStroke();
+
+	for (let i = 0; i < rects.length; i++) {
+		let s = rects[i];
+
+		if (s.size < 0.05 || random() < 0.005) {
+			s.x = cos((frameCount + i) * 0.01) * width;
+			s.y = sin((frameCount + i) * 0.01) * height;
+			s.size = 50;
+		} else {
+			s.x = lerp(s.x, 0, s.speed);
+			s.y = lerp(s.y, 0, s.speed);
+			s.size -= s.speed * s.size;
+		}
+
+		square(s.x, s.y, s.size);
+	}
+	
+	fill(1, 0.5);
+	textSize(24);
+	text('q5.js', halfWidth - 10, halfHeight - 10);
+}
